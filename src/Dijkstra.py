9c484cd30893bb116554c7b4fa726446dabfc904
Forrest Cai <forrestcai35@gmail.com>
2024-09-19 16:37:04 -0400

    Set up project structure

--- src/Dijkstra.py
+++ src/Dijkstra.py
@@ -1,6 +1,22 @@
+"""
+Basic Implementation of Dijstras Algorithm 
+Author: Forrest Cai
+"""
 
 import Graph 
 
 def Dijkstras(graph,s,d):
   """
+  Impementation of Dijstras Algorithm.
+
+  Parameter graph: 
+
+  Parameter s:
+  
+  Parameter d:
+  """
+
+def Dijkstras_Neg(graph,s,d):
+  """
+  Implementation of Dijkstas that accounts for negative edges
   """
